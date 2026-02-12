fb524b29487690514852a553ce59901365d00462
Your Name <you@example.com>
2026-02-12 09:14:54 +0000

    vars added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "KKE_ENV" {
+  value  =    
+}
+variable "KKE_DYNAMODB_TABLE_NAME" {
+  value  =    
+}
+variable "KKE_SECRET_NAME" {
+  value  =    
+}
+variable "KKE_SECRET_VALUE" {
+  value  =    
+}
+variable "KKE_ELASTICSEARCH_DOMAIN" {
+  value  =    
+}
